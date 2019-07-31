e4ce32d08ed227632153bf93ec26bcc6d4e506cb
FrankTheTank <FrankTheTank>
2019-07-31 17:12:32 +0200

    Added new Asset Package

---
+++ Frontend/MDC/Assets/Humans - Pixel Art Pack/Demo/projectileScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class projectileScript : MonoBehaviour {
+
+    [SerializeField] float speed = 1.0f;
+
+    private Rigidbody2D body2d;
+
+	// Use this for initialization
+	void Start () {
+        body2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        body2d.velocity = new Vector2(speed * transform.localScale.x, body2d.velocity.y);
+    }
+
+}
