09ea8947ad6ab0c517f89aacbe91a668327e71a5
jannis <jannis@JANNIS-UNIBOOK>
2019-09-02 14:43:12 +0200

    Game.config & Launcher
    - Game.config for test project added
    - Launcher adjusted for demo.

--- Backend/CommandPatternLauncher/src/Launcher.cs
+++ Backend/CommandPatternLauncher/src/Launcher.cs
@@ -1,11 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.IO;
-using System.Linq;
 using System.Threading;
 using MDC.Client;
-using MDC.Server;
 
 class Launcher
 {
@@ -16,40 +11,83 @@ class Launcher
     static void Main(string[] args)
     {
         ClientProgram myFirstClient = new ClientProgram();
-        // MasterServer myServer = new MasterServer();
 
-        Thread serverThread = new Thread(new ThreadStart(() => ServerInteraction()));
+        Thread serverThread = new Thread(new ThreadStart(() => MasterServer.StartServer()));
         serverThread.Start();
+
+        System.Threading.Thread.Sleep(500);
 
         myFirstClient.ConnectToServer();
         myFirstClient.CreateNewGame();
+        // myFirstClient.DisconnectFromServer();
+/* 
+        System.Threading.Thread.Sleep(500);
 
         ClientProgram mySecondClient = new ClientProgram();
         mySecondClient.ConnectToServer();
         mySecondClient.ConnectToGame(myFirstClient.GameSession_ID);
+        // try
+        // {
+        //     mySecondClient.ConnectToGame("4711");
+        // }
+        // catch (System.Exception)
+        // {
+
+        //     throw;
+        // }
+
+        // //############################################
+        // ClientProgram elChappo01 = new ClientProgram();
+        // elChappo01.ConnectToServer();
+        // elChappo01.CreateNewGame();
+        // System.Threading.Thread.Sleep(500);
+        // ClientProgram elChappo02 = new ClientProgram();
+        // elChappo02.ConnectToServer();
+        // //############################################
 
         ClientProgram myThirdClient = new ClientProgram();
         myThirdClient.ConnectToServer();
+
+        // elChappo02.ConnectToGame(elChappo01.GameSession_ID);
         myThirdClient.ConnectToGame(myFirstClient.GameSession_ID);
 
         ClientProgram myFourthClient = new ClientProgram();
         myFourthClient.ConnectToServer();
-        myFourthClient.ConnectToGame(myFirstClient.GameSession_ID);
+        myFourthClient.ConnectToGame(myFirstClient.GameSession_ID); */
 
-        //myFirstClient.CreateNewPlayerForSession("Snoop Dog", MDC.CharacterClasses.MeleeFighter);
-        //mySecondClient.CreateNewPlayerForSession("Tux", MDC.CharacterClasses.RangeFighter);
-        //myThirdClient.CreateNewPlayerForSession("Franz", MDC.CharacterClasses.MeleeFighter);
 
         // ClientProgram myFifthClient = new ClientProgram();
         // myFifthClient.ConnectToServer();
         // myFifthClient.ConnectToGame(myFirstClient.GameSession_ID);
-    }
 
-    /// <summary>
-    /// Thread implementation for clients. Waits for commands from client. 
-    /// </summary>
-    private static void ServerInteraction()
-    {
-        MasterServer.StartServer();
+        /* 
+
+        myFirstClient.CreateNewPlayerForSession("Snoop Dog", MDC.CharacterClass.MeleeFighter);
+        mySecondClient.CreateNewPlayerForSession("Tux", MDC.CharacterClass.RangeFighter);
+        myThirdClient.CreateNewPlayerForSession("Franz", MDC.CharacterClass.MeleeFighter);
+        myFourthClient.CreateNewPlayerForSession("Pepe", MDC.CharacterClass.MeleeFighter);
+
+        myFirstClient.StartCreatedGame();
+
+        myFirstClient.MovePlayer(1, 0);
+        myFirstClient.MovePlayer(2, 0);
+        myFirstClient.MovePlayer(2, 1);
+        // myFirstClient.EndTurn();
+        myFirstClient.MovePlayer(2, 2);
+        myFirstClient.MovePlayer(3, 2);
+
+        System.Threading.Thread.Sleep(2000);
+        mySecondClient.MovePlayer(18, 2);
+
+*/
+        // System.Threading.Thread.Sleep(5000);
+        // myFirstClient.DisconnectFromServer();
+        while (true)
+        {
+
+        }
+        // ClientProgram myFifthClient = new ClientProgram();
+        // myFifthClient.ConnectToServer();
+        // myFifthClient.ConnectToGame(myFirstClient.GameSession_ID);
     }
 }
