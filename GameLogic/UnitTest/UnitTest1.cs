213b6e1e6fbdbe969f9d431465f868acdea69ab5
jannis <jannis@JANNIS-UNIBOOK>
2019-09-18 16:11:05 +0200

    MS-UnitTest project added to GameLogic

---
+++ GameLogic/UnitTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+using GameLogic.MDC.Gamedata.PlayerType;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    [TestClass]
+    public class UnitTestGeneral
+    {
+        public const int MAX_PLAYER = 4;
+
+        [TestMethod]
+        public void NumberOfLife()
+        {
+            Hero player1 = new Hero("hero", new Knight(), 10, 10);
+            Monster player2 = new Monster("monster", new Archer(), 10, 19);
+
+            Assert.AreEqual(5, player1.Life);
+            Assert.AreEqual(3, player2.Life);
+        }
+    }
+}
