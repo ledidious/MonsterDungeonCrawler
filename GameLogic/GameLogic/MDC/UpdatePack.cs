58d690c978462c6416e571bb67891993cff31e55
jannis <jannis@JANNIS-UNIBOOK>
2019-09-18 12:07:52 +0200

    UpdatePack optimiert
    -New ADT created, which allows a mapping of player objects with
    ClientIDs.

--- GameLogic/GameLogic/MDC/UpdatePack.cs
+++ GameLogic/GameLogic/MDC/UpdatePack.cs
@@ -8,23 +8,32 @@ namespace GameLogic.MDC
     [Serializable]
     public class UpdatePack
     {
-        //public List<Player> PlayerList { get; }
-        //public List<string> ListOfClient_IDs { get; }
-        public Dictionary<string, Player> PlayerOfTheGame { get; }
+        public List<PlayerClientMapping> PlayerList { get; }
         public Field[,] PlayingField { get; }
         public List<Field> TrapList { get; }
         public string activeScene { get; }
         public int hash { get; }
 
-        public UpdatePack(Dictionary<string, Player> playerOfTheGame, Field[,] playingField, List<Field> trapList, string activeScene)
+        public UpdatePack(List<PlayerClientMapping> playerList, Field[,] playingField, List<Field> trapList, string activeScene)
         {
-            //this.PlayerList = playerList;
-            this.PlayerOfTheGame = playerOfTheGame;
+            this.PlayerList = playerList;
             this.PlayingField = playingField;
             this.TrapList = trapList;
             this.activeScene = activeScene;
-            //this.ListOfClient_IDs = listOfClient_IDs;
             this.hash = this.GetHashCode();
+        }
+    }
+
+    [Serializable]
+    public class PlayerClientMapping
+    {
+        public string Client_ID { get; }
+        public Player Player { get; }
+
+        public PlayerClientMapping(string client_ID, Player player)
+        {
+            this.Client_ID = client_ID;
+            this.Player = player;
         }
     }
 }
